9ea52210d787fff910957d84ebf3296609c89f44
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-17 21:56:52 +0530

    Update PerfectSquare.cpp

--- PerfectSquare.cpp
+++ PerfectSquare.cpp
@@ -1,2 +1,18 @@
 #include<iostream>
 using namespace std;
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int count=0;
+        for(long long int i=0; i*i<=num;i++){
+            if(i*i==num){
+                count++;
+            }
+        }
+        if (count==1)
+        return true;
+        else
+        return false;
+    }
+};
