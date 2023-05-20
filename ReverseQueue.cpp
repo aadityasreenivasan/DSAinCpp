06c4ddb69c73c70695f53369d287d572fac0bc25
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-20 21:35:35 +0530

    Update ReverseQueue.cpp

--- ReverseQueue.cpp
+++ ReverseQueue.cpp
@@ -26,3 +26,19 @@ void Show (queue &q)
       cout << a << " ";
   }
 }
+int main ()
+{
+  queue q ;
+  q.push (1);
+  q.push (2);
+  q.push (3);
+  q.push (4);
+  q.push (5);
+  q.push (6);
+  cout << "At first the queue is: ";
+  Show (q);
+  cout << endl;
+  Reverse_Queue_using_Stack (q);
+  cout << "After reversing the queue using stack, now to queue: ";
+  Show (q);
+}
