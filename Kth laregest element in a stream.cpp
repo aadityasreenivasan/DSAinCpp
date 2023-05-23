2b8b30b8cc35594a749287dbd60a8bc9071f55bb
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-23 13:58:39 +0530

    Update Kth laregest element in a stream.cpp

--- Kth laregest element in a stream.cpp
+++ Kth laregest element in a stream.cpp
@@ -16,4 +16,18 @@ public:
        
         
     }
-    
+     int add(int val) {
+        
+       pq.push(val);
+        while(pq.size()>sz){
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
