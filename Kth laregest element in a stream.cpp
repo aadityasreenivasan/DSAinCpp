799b8787794973ae2cde137d31a125074c7ed6ff
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-23 13:57:41 +0530

    Update Kth laregest element in a stream.cpp

--- Kth laregest element in a stream.cpp
+++ Kth laregest element in a stream.cpp
@@ -1,3 +1,19 @@
    //  the TC of the code is O(n*log(k)) for initialization and O(log(k)) for each add() operation.
    // The space complexity is O(k).
+class KthLargest {
+public:
+     
+    priority_queue<int,vector<int>,greater<int>> pq;
+    int sz;
+    KthLargest(int k, vector<int>& nums) {
 
+         sz = k;
+
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+        }
+
+       
+        
+    }
+    
