fde94c9b381e35ce982290102f93dee90c4e70d1
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-26 08:30:03 +0530

    Update PerfectSquare.cpp

--- PerfectSquare.cpp
+++ PerfectSquare.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 class Solution {
 public:
+    //funtion which returns true or false according to the number if it is a perfect square
     bool isPerfectSquare(int num) {
         int count=0;
         for(long long int i=0; i*i<=num;i++){
