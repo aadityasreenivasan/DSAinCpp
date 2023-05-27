abb723282d7f3981aa36838d08499e56fc45f1e6
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-27 21:39:08 +0530

    Create Happy Number.cpp

---
+++ Happy Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+private:
+    int nextNumber(int n){
+        int newNumber = 0;
+        while(n!=0){
+            int num = n%10;
+            newNumber += num*num;
+            n = n/10;
+        }
+        return newNumber;
+    }
