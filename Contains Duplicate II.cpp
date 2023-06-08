394fb17c0c3c8e23b0ed4263f0d6b77baef4460e
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-08 09:27:23 +0530

    Create Contains Duplicate II.cpp

---
+++ Contains Duplicate II.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int,int> map;
+
+        for(int i=0;i<n;i++)
+        {
+            if(map.count(nums[i]))
+            {
+                if(abs(i-map[nums[i]])<=k)
+                return true;
+            }
+            map[nums[i]]=i;
+        }
+        return false;
+    }
+};
