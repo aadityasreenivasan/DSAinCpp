a88274c7ca7a4a8e7e7a9637a9c18c124c9e549f
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-12 11:10:02 +0530

    Update TCSprac.cpp

--- TCSprac.cpp
+++ TCSprac.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+A coding competition was conducted in company with e employees.
+Data of employees who participated and who did not participate in the competition is available.
+There were three problems in the coding competition. Data as mentioned below is available
+The number of employees who have solved only the first, only the second and only the third problem are equal.
+p1 employees who solved the first ,second problem. p2 employees who solved the second, third problem. 
+p3 employees who solved the third first problem. g employees who solved all the 3 problems. 
+r employees who did not participate in competition.
+
+*/
+
 void helper(int p1, int p2, int p3, int q, int e, int r)
 {
     int second=e-r+q+q-p1-p2-p3;
