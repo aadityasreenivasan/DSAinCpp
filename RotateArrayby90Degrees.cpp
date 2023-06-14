fd893e185ac23aadbaa2cc8998a64f2ec88ac45c
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-14 11:02:01 +0530

    Update RotateArrayby90Degrees.cpp

--- RotateArrayby90Degrees.cpp
+++ RotateArrayby90Degrees.cpp
@@ -25,3 +25,26 @@ int main()
             swap(arr[i][j], arr[j][i]);
         }
     }
+    
+    //reversing rows of the transpose array
+    int start=0;
+    int end=n-1;
+    for(int i=0;i<n;i++)
+    {
+        swap(arr[i][start], arr[i][end]);
+        start++;
+        end--;
+    }
+    
+    //printing output array
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<arr[i][j]<<"";
+        }
+        cout<<endl;
+    }
+    return 0;
+    
+}
