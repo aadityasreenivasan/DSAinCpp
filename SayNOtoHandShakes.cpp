5f79a1ec60bddb55b1a4d2a7bfdf083ec9af6649
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-22 14:44:03 +0530

    Create SayNOtoHandShakes.cpp

---
+++ SayNOtoHandShakes.cpp
@@ -0,0 +1,8 @@
+/*
+Before the outbreak of corona virus to the world, a meeting happened in a room in wuhan.
+A person who attended that meeting had covid-19 and no one in the room knew it.
+At the end of the meeting everyone started shaking hands with everyone else in the room. 
+Given the fact that any two persons shake hands atleast once. 
+Can you tell the total count of
+handshakes happened there
+*/
