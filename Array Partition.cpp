5e47bc74a6faf3b55bde65bce51c4cb83614f25a
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-10-03 10:34:49 +0530

    Update Array Partition.cpp

--- Array Partition.cpp
+++ Array Partition.cpp
@@ -16,3 +16,13 @@ Explanation: All possible pairings (ignoring the ordering of elements) are:
 3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
 So the maximum possible sum is 4.
 */
+
+int arrayPairSum(vector<int>& nums) {
+        int sum=0;
+        sort(nums.begin(), nums.end());
+        for(int i=0;i<nums.size();i+=2)
+        {
+            sum+=nums[i];
+        }
+        return sum;
+    }
