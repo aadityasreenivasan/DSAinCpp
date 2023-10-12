58d5b63a070bb234a7ccaddcb5c030f50101ab8c
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-10-12 11:34:02 +0530

    Update Search Insert Position.cpp

--- Search Insert Position.cpp
+++ Search Insert Position.cpp
@@ -1,6 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
 int searchInsert(vector<int>& nums, int target) {
         int l=0;
         int r=nums.size()-1;
