f9e977465ca73ba33040a75bcd24573fbb5bd469
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-10-13 15:29:19 +0530

    Create Min Cost Climbing Stairs.cpp

---
+++ Min Cost Climbing Stairs.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minCostClimbingStairs(vector<int>& cost) {
+        int s=cost.size();
+        for(int i=2;i<cost.size();i++)
+        {
+            cost[i]+=min(cost[i-1], cost[i-2]);
+        }
+        return min(cost[s-1], cost[s-2]);
+    }
