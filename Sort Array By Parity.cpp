7a88fc773bd9be6674055b9ccad2f6787ef693e7
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-10-15 10:53:19 +0530

    Update Sort Array By Parity.cpp

--- Sort Array By Parity.cpp
+++ Sort Array By Parity.cpp
@@ -1,6 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+
+Return any array that satisfies this condition.
+*/
+
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
