d547c6da6c9db7cb93d24e1392237db5f7aa35ad
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-10-17 22:48:42 +0530

    Create Assign Cookies.cpp

---
+++ Assign Cookies.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int satisfied=0;
+
+        int a=g.size()-1;
+        int b=s.size()-1;
+
+        while(a>=0 && b>=0){
+            if(s[b]>=g[a]){
+                satisfied++;
+                a--;
+                b--;
+            }else{
+                a--;
+            }
+        }
+
+        return satisfied;
+    }
+};
