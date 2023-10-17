c224715e367881bf60fdb747f136ed175c7b92a7
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-10-17 22:47:17 +0530

    Update Monotonic Array.cpp

--- Monotonic Array.cpp
+++ Monotonic Array.cpp
@@ -1,6 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
+/*
+An array is monotonic if it is either monotone increasing or monotone decreasing.
 
+An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
+
+Given an integer array nums, return true if the given array is monotonic, or false otherwise.
+*/
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
